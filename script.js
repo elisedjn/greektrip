d6303ad7312135f4a7923b88e69646290e157af6
Elise Djn <elisedjn@protonmail.com>
2020-10-16 09:57:14 +0300

    sounds and pop up fixed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Query Selector
 const startBtn = document.querySelector("#start-btn");
 
+const popUpContainer = document.querySelector("#popUpContainer");
 const popUp = document.querySelector("#popUp");
 const popUpText = document.querySelector(".popUp-text");
 const popUpBtn = document.querySelector(".continue-btn");
@@ -31,23 +32,27 @@ music = new Audio("./public/zorba.mp3");
 music.volume = 0.2;
 music.loop = true;
 donkeySound = new Audio("./public/donkey.wav");
-donkeySound.volume = 0.5;
+donkeySound.volume = 0.8;
 seagullSound = new Audio("./public/seagull.wav");
-seagullSound.volume = 0.5;
+seagullSound.volume = 0.8;
 catSound = new Audio("./public/cat.wav");
-catSound.volume = 0.5;
+catSound.volume = 0.8;
 
 // Event Listeners
 startBtn.addEventListener("click", (event) => {
   music.play()
 })
 
+popUpContainer.addEventListener("click", (event) => {
+  popUpContainer.style.display = "none";
+})
+
 popUpBtn.addEventListener("click", (event) => {
-  popUp.style.display = "none";
+  popUpContainer.style.display = "none";
 })
 
 boat.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> 6h00 du matin, Athènes.</h4>
   <p class="story">Les yeux encore un peu collés, nous partons de l’hôtel pour prendre le métro. Direction le port où le ferry nous attend. Le bateau est grand, avec différents salons. On choisi les canapés les plus confortables, à l’avant du bateau. Il faut qu’on soit bien, la traversée dure plus de 7 heures! Finalement ça passe assez vite, quelques boissons au bar, un burger immonde au seul snack du bateau,  un film, du repos et nous voilà au port de Santorini, petite esplanade en bas d’une falaise impressionnante. Les habitations sont tout en haut, au bout d’une route sinueuse que nous parcourons en bus.</p>
@@ -57,10 +62,12 @@ boat.addEventListener("click", (event) => {
 
 donkey.addEventListener("click", (event) => {
   donkeySound.play()
+  music.volume=0.1
+  setTimeout(() => {music.volume=0.2}, 6000 )
 })
 
  cat.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Vue depuis Oia </h4>
   <div class="popup-image"><img src="./public/photos/oia-elise.jpg" alt = "Elise à Oia" /> </div>
@@ -68,7 +75,7 @@ donkey.addEventListener("click", (event) => {
 })
 
 cat2.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Steffel à Fira </h4>
   <div class="popup-image"><img src="./public/photos/stef.jpg" alt = "Steffel à Fira" /> </div>
@@ -77,17 +84,19 @@ cat2.addEventListener("click", (event) => {
 
 seagull.addEventListener("click", (event) => {
   seagullSound.play();
+  music.volume=0.1
+  setTimeout(() => {music.volume=0.2}, 15000 )
 })
 
 sun.addEventListener("click", (event) => {
-  popUp.style.display = "block"
+  popUpContainer.style.display = "block"
   popUpText.innerHTML = `
   <div class="popup-image"><img src="./public/photos/sunset.jpg" alt = "Sunset" /> </div>
   `
 })
 
 plant.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Un peu trop petite... </h4>
   <div class="popup-image"><img src="./public/photos/petite.jpg" alt = "Elise petite" /> </div>
@@ -95,7 +104,7 @@ plant.addEventListener("click", (event) => {
 })
 
 palmtree.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Elise à Oia </h4>
   <div class="popup-image"><img src="./public/photos/oia-re-elise.jpg" alt = "Elise à Oia" /> </div>
@@ -103,7 +112,7 @@ palmtree.addEventListener("click", (event) => {
 })
 
 palmtreeSmall.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Elise à Oia </h4>
   <div class="popup-image"><img src="./public/photos/oia-re-elise.jpg" alt = "Elise à Oia" /> </div>
@@ -111,7 +120,7 @@ palmtreeSmall.addEventListener("click", (event) => {
 })
 
 greek.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> La Caverne d'Ali Baba </h4>
   <p class="story"> surnom donné à la supérette en haut de la rue de l’hôtel. Tenue par un monsieur au gros ventre et aux vêtements d’une propreté douteuse, chaque cm2 de la boutique comporte quelque chose à acheter, du sol au plafond c’est rempli! En plus la musique religieuse donne une impression de chapelle dédiée à la nourriture! <br/>
@@ -120,7 +129,7 @@ greek.addEventListener("click", (event) => {
 })
 
 selfie.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Oia </h4>
   <div class="popup-image"><img src="./public/photos/oia-selfie.jpg" alt = "Selfie à Oia" /> </div>
@@ -128,7 +137,7 @@ selfie.addEventListener("click", (event) => {
 })
 
 filming.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Oia </h4>
   <div class="popup-image"><img src="./public/photos/oia.jpg" alt = "Oia" /> </div>
@@ -136,7 +145,7 @@ filming.addEventListener("click", (event) => {
 })
 
 walking.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <p class="story">Nous logeons dans le village touristique de Fira. Depuis celui-ci, un sentier d’une dizaine de kilomètres longe les falaises et rejoint le village encore plus touristique de Oia. On ne se sent pas de faire les 10km à pied en plein soleil (surtout que les 5 derniers km se font dans des espèces de pierriers avec pas un gramme d’ombre) mais on décide de suivre le sentier sur presque la moitié, de prendre un bus pour rentrer à Fira puis d’aller à Oia le soir en bus pour voir le coucher de soleil. Sauf que les bus grecs, c’est pas super bien expliqué. Et en fait, depuis l’arrêt où on était, pour aller à Fira, il fallait prendre le bus qui va à Oia puis rester dans celui-ci pour faire la route de retour qui passe par l’autre côté de l’île. Bon, comme on était à Oia, on a décidé de visiter le village directement. C’est très joli, mais rempli d’instagrammeuses en longues robes et en talons qui font des photos pour les réseaux sociaux. On ose même pas imaginer le monde qu’il doit y avoir en période touristique (et sans le covid).</p>
   <div class="popup-image"><img src="./public/photos/walk.jpg" alt = "Marche" /> </div>
@@ -144,7 +153,7 @@ walking.addEventListener("click", (event) => {
 })
 
 walking2.addEventListener("click", () => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Fira by Night </h4>
   <div class="popup-image"><img src="./public/photos/fira-night.jpg" alt = "Fira by night" /> </div>
@@ -152,7 +161,7 @@ walking2.addEventListener("click", () => {
 })
 
 walking3.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <p class="story">Il y a très très (mais alors très) longtemps, Santorini était une seule grande île avec un volcan au milieu. Il y a un tout petit peu moins longtemps, une énorme éruption a fait s’effondrer le volcan (bon elle a aussi causé un giga tsunami et les cendres qui sont montées dans l’atmosphère ont obstrué partiellement la lumière du soleil, entrainant une baisse des températures de 2 à 3 degrés sur toute la planète) créant ainsi l’archipel comme on le connait aujourd’hui : deux îles centrales inhabitées avec des cratères (plusieurs inactifs et un encore actif) et des îles tout autour, plus ou moins grandes, habitées ou non. <br />
   Valeureux aventuriers que nous sommes, nous avons bravé tous les dangers pour marcher sur l’ile de Nea Kameni qui est en fait la partie émergée du volcan actif. Nous avons pu marcher sur les pierres volcaniques, voir 3 cratères inactifs et un cratère actif (dernière éruption en date en 1950). Bon ok, on était en visite guidée et encadrée… et le cratère actif ressemble seulement à une depression dans le terrain avec quelques fumeroles et une odeur de souffre. Mais quand même, on a marché sur un volcan actif!</p>
@@ -161,7 +170,7 @@ walking3.addEventListener("click", (event) => {
 })
 
 fish.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Fira </h4>
   <div class="popup-image"><img src="./public/photos/fira.jpg" alt = "Fira" /> </div>
@@ -170,17 +179,19 @@ fish.addEventListener("click", (event) => {
 
 sleepingcat.addEventListener("click", (event) => {
   catSound.play();
+  music.volume=0.1
+  setTimeout(() => {music.volume=0.2}, 3000 )
 })
 
 seagull2.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <div class="popup-image"><img src="./public/photos/wind.jpg" alt = "Ca vente!" /> </div>
   `
 })
 
 gyros.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <h4 class="picture-title"> Aaaaah les gyros! </h4>
   <p class="story">La nouvelle passion de Stef! Ça ressemble aux kebabs que l’on a en France, mais avec de la viande de porc ou de poulet, du tzatziki (crème et concombre) et dans du pain pita (plus moelleux que les pains en France). C’est pas cher, et c’est très bon! Bon, bien sûr on aime aussi la moussaka! </p>
@@ -188,7 +199,7 @@ gyros.addEventListener("click", (event) => {
 })
 
 smoke.addEventListener("click", (event) => {
-  popUp.style.display = "block";
+  popUpContainer.style.display = "block";
   popUpText.innerHTML = `
   <p class="story">Notre voilier (aux voiles repliées et au moteur tournant à fond) s’approche de l’île de Palia Kameni, petite île volcanique au centre de l’archipel de Santorini. Avec quelques autres passagers (touristes allemands et français pour la plupart) nous sautons à l’eau depuis le pont. En quelques brasses nous rejoignons une baie à l’eau étrangement orange/marron et dont la température avoisine les 35 degrés (d’après la brochure, parce que d’après nos estimations c’était plus autour de 25/30). On se prélasse dans cette eau très chargée en boue souffrée (d’où la couleur) et chauffée par l’activité volcanique. C’est super bon pour la peau, mais on ressort orange. Je me suis demandé si Trump venait se faire des teintures ici…  </p>
   `
